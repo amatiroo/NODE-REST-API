65f376de0487654e9f68dcb81e0cec8577df1aa6
amatiroo <rooamati@gmail.com>
2023-05-28 13:25:45 -0500

    Initial Commit

---
+++ routes/posts.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const Post = require("../models/Post")
+
+//create a post
+
+router.post("/",async (req,res)=>{
+    const newPost = new Post(req.body);
+
+    try{
+
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//update a post
+
+router.put("/:id",async (req,res)=>{
+
+    try{
+        const post = await Post.findById(req.params.id)
+
+        if (post.userId===req.body.userId){
+    
+        } else {
+            res.status(403).json("you can only update your post")
+        }
+    } catch(err){
+        res.status(500).json(err)
+    }
+  
+})
+//delete a post
+//like a post
+//get a post
+//get timeline post
+
+
+module.exports = router
